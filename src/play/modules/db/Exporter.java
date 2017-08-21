428a627ee8bd5467e42a821903b0b6be2b97d4c9
Chris Webb <chris@spinthewebb.com>
2017-08-21 22:54:41 +0100

    1: Support Play 1.5 (Hibernate 5.2)
    Task-Url: https://github.com/tazmaniax/play--database/issues/1
    
    Code formatting

--- src/play/modules/db/Exporter.java
+++ src/play/modules/db/Exporter.java
@@ -53,13 +53,16 @@ import play.vfs.VirtualFile;
 
 public class Exporter {
 
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-
 		File root = new File(System.getProperty("application.path"));
 		String id = System.getProperty("play.id", "");
 //		Play.init(root, id);
 		startDBPlugin(root, id);
-
+		
 //		boolean script = true;
 		boolean drop = false;
 		boolean create = false;
@@ -70,10 +73,10 @@ public class Exporter {
 		String propFile = null;
 		boolean format = true;
 		String delim = ";";
-
+		
 		EnumSet<TargetType> targetTypes = EnumSet.noneOf(TargetType.class);
 		targetTypes.add(TargetType.STDOUT);
-
+		
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].startsWith("--")) {
 				if (args[i].equals("--drop")) {
@@ -128,14 +131,14 @@ public class Exporter {
 		}
 		
 		MetadataSources metadata = new MetadataSources(serviceRegistryBuilder.build());
-			    
+		
 		// [...] adding annotated classes to metadata here...
 //		List<Class> entities = Play.classloader.getAnnotatedClasses(Entity.class);
 		List<Class> entities = entityClasses(dbName);
 		for (Class _class : entities) {
 			metadata.addAnnotatedClass(_class);
 		}
-
+		
 		SchemaExport se = new SchemaExport()
 				.setHaltOnError(halt)
 				.setDelimiter(delim)
@@ -150,7 +153,7 @@ public class Exporter {
 		}
 		
 		Action action = (drop && create) ? Action.BOTH : drop ? Action.DROP : create ? Action.CREATE : Action.NONE;
-
+		
 		// se.execute(script, export, drop, create);
 		se.execute(targetTypes, action, metadata.buildMetadata());
 	}
@@ -159,94 +162,94 @@ public class Exporter {
 	 * NOTE: This has been copied from play.db.Evolutions.main(String[])
 	 */
 	private static void startDBPlugin(File root, String id) {
-        /** Start the DB plugin **/
+		/** Start the DB plugin **/
 		Play.id = id;
-	    Play.started = false;
-	    Play.applicationPath = root;
-        Play.guessFrameworkPath();
-        Play.readConfiguration();
-        Play.classes = new ApplicationClasses();
-        Play.classloader = new ApplicationClassloader();
-        
-        VirtualFile appRoot = VirtualFile.open(Play.applicationPath);
-        Play.roots.clear();
-        Play.roots.add(appRoot);
-        Play.javaPath.clear();
-        Play.javaPath.add(appRoot.child("app"));
-        Play.javaPath.add(appRoot.child("conf"));
-
-        Play.loadModules(VirtualFile.open(Play.applicationPath));
-
-        Logger.init();
-        Logger.setUp("ERROR");
-        new DBPlugin().onApplicationStart();
+		Play.started = false;
+		Play.applicationPath = root;
+		Play.guessFrameworkPath();
+		Play.readConfiguration();
+		Play.classes = new ApplicationClasses();
+		Play.classloader = new ApplicationClassloader();
+		
+		VirtualFile appRoot = VirtualFile.open(Play.applicationPath);
+		Play.roots.clear();
+		Play.roots.add(appRoot);
+		Play.javaPath.clear();
+		Play.javaPath.add(appRoot.child("app"));
+		Play.javaPath.add(appRoot.child("conf"));
+		
+		Play.loadModules(VirtualFile.open(Play.applicationPath));
+		
+		Logger.init();
+		Logger.setUp("ERROR");
+		new DBPlugin().onApplicationStart();
 	}
 	
-    /**
-     * @param dbName
-     * @param dbConfig
-     * @return
-     * 
-     * NOTE: This has been copied from play.db.jpa.JPAPlugin.properties(String dbName, Configuration dbConfig)
-     */
-    private static Properties properties(String dbName, Configuration dbConfig) {
-        Properties properties = new Properties();
-        properties.putAll(dbConfig.getProperties());
-        properties.put("javax.persistence.transaction", "RESOURCE_LOCAL");
-        properties.put("javax.persistence.provider", "org.hibernate.ejb.HibernatePersistence");
-        properties.put("hibernate.dialect", JPAPlugin.getDefaultDialect(dbConfig, dbConfig.getProperty("db.driver")));
-
-        if (!dbConfig.getProperty("jpa.ddl", Play.mode.isDev() ? "update" : "none").equals("none")) {
-            properties.setProperty("hibernate.hbm2ddl.auto", dbConfig.getProperty("jpa.ddl", "update"));
-        }
-
-        properties.put("hibernate.connection.datasource", DB.getDataSource(dbName));
-        
-        return properties;
-    }
-    
-    /**
-     * @param dbName
-     * @return
-     * 
-     * NOTE: This has been copied from play.db.jpa.JPAPlugin.entityClasses(String dbName)
-     */
-    private static List<Class> entityClasses(String dbName) {
-        List<Class> entityClasses = new ArrayList<>();
-        
-        List<Class> classes = Play.classloader.getAnnotatedClasses(Entity.class);
-        for (Class<?> clazz : classes) {
-            if (clazz.isAnnotationPresent(Entity.class)) {
-                // Do we have a transactional annotation matching our dbname?
-                PersistenceUnit pu = clazz.getAnnotation(PersistenceUnit.class);
-                if (pu != null && pu.name().equals(dbName)) {
-                    entityClasses.add(clazz);
-                } else if (pu == null && JPA.DEFAULT.equals(dbName)) {
-                    entityClasses.add(clazz);
-                }                    
-            }
-        }
-
-        // Add entities
-        String[] moreEntities = Play.configuration.getProperty("jpa.entities", "").split(", ");
-        for (String entity : moreEntities) {
-            if (entity.trim().equals("")) {
-                continue;
-            }
-            try {
-                Class<?> clazz = Play.classloader.loadClass(entity);  
-                // Do we have a transactional annotation matching our dbname?
-                PersistenceUnit pu = clazz.getAnnotation(PersistenceUnit.class);
-                if (pu != null && pu.name().equals(dbName)) {
-                    entityClasses.add(clazz);
-                } else if (pu == null && JPA.DEFAULT.equals(dbName)) {
-                    entityClasses.add(clazz);
-                }         
-            } catch (Exception e) {
-                Logger.warn(e, "JPA -> Entity not found: %s", entity);
-            }
-        }
-        
-        return entityClasses;
-    }
+	/**
+	 * @param dbName
+	 * @param dbConfig
+	 * @return
+	 * 
+	 * NOTE: This has been copied from play.db.jpa.JPAPlugin.properties(String dbName, Configuration dbConfig)
+	 */
+	private static Properties properties(String dbName, Configuration dbConfig) {
+		Properties properties = new Properties();
+		properties.putAll(dbConfig.getProperties());
+		properties.put("javax.persistence.transaction", "RESOURCE_LOCAL");
+		properties.put("javax.persistence.provider", "org.hibernate.ejb.HibernatePersistence");
+		properties.put("hibernate.dialect", JPAPlugin.getDefaultDialect(dbConfig, dbConfig.getProperty("db.driver")));
+		
+		if (!dbConfig.getProperty("jpa.ddl", Play.mode.isDev() ? "update" : "none").equals("none")) {
+			properties.setProperty("hibernate.hbm2ddl.auto", dbConfig.getProperty("jpa.ddl", "update"));
+		}
+		
+		properties.put("hibernate.connection.datasource", DB.getDataSource(dbName));
+		
+		return properties;
+	}
+	
+	/**
+	 * @param dbName
+	 * @return
+	 * 
+	 * NOTE: This has been copied from play.db.jpa.JPAPlugin.entityClasses(String dbName)
+	 */
+	private static List<Class> entityClasses(String dbName) {
+		List<Class> entityClasses = new ArrayList<>();
+		
+		List<Class> classes = Play.classloader.getAnnotatedClasses(Entity.class);
+		for (Class<?> clazz : classes) {
+			if (clazz.isAnnotationPresent(Entity.class)) {
+				// Do we have a transactional annotation matching our dbname?
+				PersistenceUnit pu = clazz.getAnnotation(PersistenceUnit.class);
+				if (pu != null && pu.name().equals(dbName)) {
+					entityClasses.add(clazz);
+				} else if (pu == null && JPA.DEFAULT.equals(dbName)) {
+					entityClasses.add(clazz);
+				}
+			}
+		}
+		
+		// Add entities
+		String[] moreEntities = Play.configuration.getProperty("jpa.entities", "").split(", ");
+		for (String entity : moreEntities) {
+			if (entity.trim().equals("")) {
+				continue;
+			}
+			try {
+				Class<?> clazz = Play.classloader.loadClass(entity);
+				// Do we have a transactional annotation matching our dbname?
+				PersistenceUnit pu = clazz.getAnnotation(PersistenceUnit.class);
+				if (pu != null && pu.name().equals(dbName)) {
+					entityClasses.add(clazz);
+				} else if (pu == null && JPA.DEFAULT.equals(dbName)) {
+					entityClasses.add(clazz);
+				}
+			} catch (Exception e) {
+				Logger.warn(e, "JPA -> Entity not found: %s", entity);
+			}
+		}
+		
+		return entityClasses;
+	}
 }
